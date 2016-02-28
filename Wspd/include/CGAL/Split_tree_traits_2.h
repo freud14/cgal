b85564818b8d765f431a67e5e821344383e0774b
Frédérik Paradis <fredy_14@live.fr>
2016-02-28 13:52:41 -0500

    Adding license to Split_tree_traits_2.h

--- Wspd/include/CGAL/Split_tree_traits_2.h
+++ Wspd/include/CGAL/Split_tree_traits_2.h
@@ -1,8 +1,28 @@
+// Copyright (c) 2016 University of Ottawa (Canada)
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+// Author(s)     : Frédérik Paradis (<fpara058@uottawa.ca>)
+
 #ifndef CGAL_SPLIT_TREE_TRAITS_2_H
 #define CGAL_SPLIT_TREE_TRAITS_2_H
 #include <CGAL/Search_traits_2.h>
 
 namespace CGAL {
+
   template <class K >
   class Construct_sphere {
   public:
