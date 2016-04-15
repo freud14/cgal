97b5fae9ddd45e171d19d7a4be7e0e6d7fac1569
Frédérik Paradis <fredy_14@live.fr>
2016-04-15 15:25:57 -0400

    Adding tests for different types of points

--- Wspd/test/Wspd/split_tree_test.cpp
+++ Wspd/test/Wspd/split_tree_test.cpp
@@ -1,5 +1,8 @@
 #include <CGAL/Simple_cartesian.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Homogeneous.h>
 #include <CGAL/Cartesian_d.h>
+#include <CGAL/Homogeneous_d.h>
 #include <CGAL/Split_tree_traits_2.h>
 #include <CGAL/Split_tree_traits_3.h>
 #include <CGAL/Split_tree_traits_d.h>
@@ -27,18 +30,58 @@ int main(int argc, char* argv[])
 #endif
 
 #ifdef CGAL_USE_GMP
-  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CGAL::Quotient<CGAL::Gmpz> > > split_tree_S_Qgmp;
+  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CGAL::Quotient<CGAL::Gmpz> > > split_tree_2_S_Qgmp;
   std::cout << "SimpleCartesian<Quotient<Gmpz> > >:     ";
-  CGAL::split_tree__batch_test(2, split_tree_S_Qgmp);
+  CGAL::split_tree__batch_test(2, split_tree_2_S_Qgmp);
 #endif
 
-  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<double> > split_tree_S_double;
+  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<double> > split_tree_2_S_double;
   std::cout << "SimpleCartesian<double>:     ";
-  CGAL::split_tree__batch_test(2, split_tree_S_double);
+  CGAL::split_tree__batch_test(2, split_tree_2_S_double);
 
-  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CORE::Expr> > split_tree_S_core;
+  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CORE::Expr> > split_tree_2_S_core;
   std::cout << "SimpleCartesian<CORE::Expr>:     ";
-  CGAL::split_tree__batch_test(2, split_tree_S_core);
+  CGAL::split_tree__batch_test(2, split_tree_2_S_core);
+
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<leda_rational> > split_tree_2_C_rational;
+  std::cout << "Cartesian<rational>:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_C_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<CGAL::Quotient<CGAL::Gmpz> > > split_tree_2_C_Qgmp;
+  std::cout << "Cartesian<Quotient<Gmpz> > >:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_C_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<double> > split_tree_2_C_double;
+  std::cout << "Cartesian<double>:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_C_double);
+
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<CORE::Expr> > split_tree_2_C_core;
+  std::cout << "Cartesian<CORE::Expr>:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_C_core);
+
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<leda_rational> > split_tree_2_H_rational;
+  std::cout << "Homogeneous<rational>:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_H_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<CGAL::Quotient<CGAL::Gmpz> > > split_tree_2_H_Qgmp;
+  std::cout << "Homogeneous<Quotient<Gmpz> > >:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_H_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<double> > split_tree_2_H_double;
+  std::cout << "Homogeneous<double>:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_H_double);
+
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<CORE::Expr> > split_tree_2_H_core;
+  std::cout << "Homogeneous<CORE::Expr>:     ";
+  CGAL::split_tree__batch_test(2, split_tree_2_H_core);
 
 
   std::cout << "In 3 dimensions:" << std::endl;
@@ -62,6 +105,46 @@ int main(int argc, char* argv[])
   std::cout << "SimpleCartesian<CORE::Expr>:     ";
   CGAL::split_tree__batch_test(3, split_tree_3_S_core);
 
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<leda_rational> > split_tree_3_C_rational;
+  std::cout << "Cartesian<rational>:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_C_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<CGAL::Quotient<CGAL::Gmpz> > > split_tree_3_C_Qgmp;
+  std::cout << "Cartesian<Quotient<Gmpz> > >:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_C_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<double> > split_tree_3_C_double;
+  std::cout << "Cartesian<double>:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_C_double);
+
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<CORE::Expr> > split_tree_3_C_core;
+  std::cout << "Cartesian<CORE::Expr>:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_C_core);
+
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<leda_rational> > split_tree_3_H_rational;
+  std::cout << "Homogeneous<rational>:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_H_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<CGAL::Quotient<CGAL::Gmpz> > > split_tree_3_H_Qgmp;
+  std::cout << "Homogeneous<Quotient<Gmpz> > >:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_H_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<double> > split_tree_3_H_double;
+  std::cout << "Homogeneous<double>:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_H_double);
+
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<CORE::Expr> > split_tree_3_H_core;
+  std::cout << "Homogeneous<CORE::Expr>:     ";
+  CGAL::split_tree__batch_test(3, split_tree_3_H_core);
+
 
   int d = 4;
   std::cout << "In d dimensions (d = 4):" << std::endl;
@@ -77,7 +160,6 @@ int main(int argc, char* argv[])
   CGAL::split_tree__batch_test(d, split_tree_d_S_Qgmp);
 #endif
 
-  typedef CGAL::Split_tree_traits_d< CGAL::Cartesian_d<double> > Traits;
   CGAL::Split_tree_traits_d< CGAL::Cartesian_d<double> > split_tree_d_S_double;
   std::cout << "Cartesian_d<double>:     ";
   CGAL::split_tree__batch_test(d, split_tree_d_S_double);
@@ -85,5 +167,27 @@ int main(int argc, char* argv[])
   CGAL::Split_tree_traits_d< CGAL::Cartesian_d<CORE::Expr> > split_tree_d_S_core;
   std::cout << "Cartesian_d<CORE::Expr>:     ";
   CGAL::split_tree__batch_test(d, split_tree_d_S_core);
+
+  // TODO: Make it work with homogeneous coordinates.
+/*#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<leda_rational> > split_tree_d_H_rational;
+  std::cout << "Homogeneous_d<rational>:     ";
+  CGAL::split_tree__batch_test(d, split_tree_d_H_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<CGAL::Quotient<CGAL::Gmpz> > > split_tree_d_H_Qgmp;
+  std::cout << "Homogeneous_d<Quotient<Gmpz> > >:     ";
+  CGAL::split_tree__batch_test(d, split_tree_d_H_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<double> > split_tree_d_H_double;
+  std::cout << "Homogeneous_d<double>:     ";
+  CGAL::split_tree__batch_test(d, split_tree_d_H_double);
+
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<CORE::Expr> > split_tree_d_H_core;
+  std::cout << "Homogeneous_d<CORE::Expr>:     ";
+  CGAL::split_tree__batch_test(d, split_tree_d_H_core);*/
+
   return 0;
 }
--- Wspd/test/Wspd/wspd_test.cpp
+++ Wspd/test/Wspd/wspd_test.cpp
@@ -1,5 +1,8 @@
 #include <CGAL/Simple_cartesian.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Homogeneous.h>
 #include <CGAL/Cartesian_d.h>
+#include <CGAL/Homogeneous_d.h>
 #include <CGAL/Split_tree_traits_2.h>
 #include <CGAL/Split_tree_traits_3.h>
 #include <CGAL/Split_tree_traits_d.h>
@@ -16,6 +19,7 @@
 #include <CGAL/CORE_Expr.h>
 
 #include <CGAL/wspd_test.h>
+#include <CGAL/WSPD.h>
 
 int main(int argc, char* argv[])
 {
@@ -27,18 +31,58 @@ int main(int argc, char* argv[])
 #endif
 
 #ifdef CGAL_USE_GMP
-  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CGAL::Quotient<CGAL::Gmpz> > > wspd_S_Qgmp;
+  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CGAL::Quotient<CGAL::Gmpz> > > wspd_2_S_Qgmp;
   std::cout << "SimpleCartesian<Quotient<Gmpz> > >:     ";
-  CGAL::wspd__batch_test(2, wspd_S_Qgmp);
+  CGAL::wspd__batch_test(2, wspd_2_S_Qgmp);
 #endif
 
-  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<double> > wspd_S_double;
+  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<double> > wspd_2_S_double;
   std::cout << "SimpleCartesian<double>:     ";
-  CGAL::wspd__batch_test(2, wspd_S_double);
+  CGAL::wspd__batch_test(2, wspd_2_S_double);
 
-  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CORE::Expr> > wspd_S_core;
+  CGAL::Split_tree_traits_2< CGAL::Simple_cartesian<CORE::Expr> > wspd_2_S_core;
   std::cout << "SimpleCartesian<CORE::Expr>:     ";
-  CGAL::wspd__batch_test(2, wspd_S_core);
+  CGAL::wspd__batch_test(2, wspd_2_S_core);
+
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<leda_rational> > wspd_2_C_rational;
+  std::cout << "Cartesian<rational>:     ";
+  CGAL::wspd__batch_test(2, wspd_2_C_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<CGAL::Quotient<CGAL::Gmpz> > > wspd_2_C_Qgmp;
+  std::cout << "Cartesian<Quotient<Gmpz> > >:     ";
+  CGAL::wspd__batch_test(2, wspd_2_C_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<double> > wspd_2_C_double;
+  std::cout << "Cartesian<double>:     ";
+  CGAL::wspd__batch_test(2, wspd_2_C_double);
+
+  CGAL::Split_tree_traits_2< CGAL::Cartesian<CORE::Expr> > wspd_2_C_core;
+  std::cout << "Cartesian<CORE::Expr>:     ";
+  CGAL::wspd__batch_test(2, wspd_2_C_core);
+
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<leda_rational> > wspd_2_H_rational;
+  std::cout << "Homogeneous<rational>:     ";
+  CGAL::wspd__batch_test(2, wspd_2_H_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<CGAL::Quotient<CGAL::Gmpz> > > wspd_2_H_Qgmp;
+  std::cout << "Homogeneous<Quotient<Gmpz> > >:     ";
+  CGAL::wspd__batch_test(2, wspd_2_H_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<double> > wspd_2_H_double;
+  std::cout << "Homogeneous<double>:     ";
+  CGAL::wspd__batch_test(2, wspd_2_H_double);
+
+  CGAL::Split_tree_traits_2< CGAL::Homogeneous<CORE::Expr> > wspd_2_H_core;
+  std::cout << "Homogeneous<CORE::Expr>:     ";
+  CGAL::wspd__batch_test(2, wspd_2_H_core);
 
 
   std::cout << "In 3 dimensions:" << std::endl;
@@ -62,6 +106,46 @@ int main(int argc, char* argv[])
   std::cout << "SimpleCartesian<CORE::Expr>:     ";
   CGAL::wspd__batch_test(3, wspd_3_S_core);
 
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<leda_rational> > wspd_3_C_rational;
+  std::cout << "Cartesian<rational>:     ";
+  CGAL::wspd__batch_test(3, wspd_3_C_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<CGAL::Quotient<CGAL::Gmpz> > > wspd_3_C_Qgmp;
+  std::cout << "Cartesian<Quotient<Gmpz> > >:     ";
+  CGAL::wspd__batch_test(3, wspd_3_C_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<double> > wspd_3_C_double;
+  std::cout << "Cartesian<double>:     ";
+  CGAL::wspd__batch_test(3, wspd_3_C_double);
+
+  CGAL::Split_tree_traits_3< CGAL::Cartesian<CORE::Expr> > wspd_3_C_core;
+  std::cout << "Cartesian<CORE::Expr>:     ";
+  CGAL::wspd__batch_test(3, wspd_3_C_core);
+
+#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<leda_rational> > wspd_3_H_rational;
+  std::cout << "Homogeneous<rational>:     ";
+  CGAL::wspd__batch_test(3, wspd_3_H_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<CGAL::Quotient<CGAL::Gmpz> > > wspd_3_H_Qgmp;
+  std::cout << "Homogeneous<Quotient<Gmpz> > >:     ";
+  CGAL::wspd__batch_test(3, wspd_3_H_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<double> > wspd_3_H_double;
+  std::cout << "Homogeneous<double>:     ";
+  CGAL::wspd__batch_test(3, wspd_3_H_double);
+
+  CGAL::Split_tree_traits_3< CGAL::Homogeneous<CORE::Expr> > wspd_3_H_core;
+  std::cout << "Homogeneous<CORE::Expr>:     ";
+  CGAL::wspd__batch_test(3, wspd_3_H_core);
+
 
   int d = 4;
   std::cout << "In d dimensions (d = 4):" << std::endl;
@@ -77,7 +161,6 @@ int main(int argc, char* argv[])
   CGAL::wspd__batch_test(d, wspd_d_S_Qgmp);
 #endif
 
-  typedef CGAL::Split_tree_traits_d< CGAL::Cartesian_d<double> > Traits;
   CGAL::Split_tree_traits_d< CGAL::Cartesian_d<double> > wspd_d_S_double;
   std::cout << "Cartesian_d<double>:     ";
   CGAL::wspd__batch_test(d, wspd_d_S_double);
@@ -85,5 +168,27 @@ int main(int argc, char* argv[])
   CGAL::Split_tree_traits_d< CGAL::Cartesian_d<CORE::Expr> > wspd_d_S_core;
   std::cout << "Cartesian_d<CORE::Expr>:     ";
   CGAL::wspd__batch_test(d, wspd_d_S_core);
+
+  // TODO: Make it work with homogeneous coordinates.
+/*#ifdef CGAL_USE_LEDA
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<leda_rational> > wspd_d_H_rational;
+  std::cout << "Homogeneous_d<rational>:     ";
+  CGAL::wspd__batch_test(d, wspd_d_H_rational);
+#endif
+
+#ifdef CGAL_USE_GMP
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<CGAL::Quotient<CGAL::Gmpz> > > wspd_d_H_Qgmp;
+  std::cout << "Homogeneous_d<Quotient<Gmpz> > >:     ";
+  CGAL::wspd__batch_test(d, wspd_d_H_Qgmp);
+#endif
+
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<double> > wspd_d_H_double;
+  std::cout << "Homogeneous_d<double>:     ";
+  CGAL::wspd__batch_test(d, wspd_d_H_double);
+
+  CGAL::Split_tree_traits_d< CGAL::Homogeneous_d<CORE::Expr> > wspd_d_H_core;
+  std::cout << "Homogeneous_d<CORE::Expr>:     ";
+  CGAL::wspd__batch_test(d, wspd_d_H_core);*/
+
   return 0;
 }
